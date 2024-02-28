3908714f84aaa1b321801a28a7554bdafce50af0
Atul Bharadwaj <tulbha2482@gmail.com>
2024-02-28 12:06:00 +0530

    Conversion of Datatypes

---
+++ 1.Variables/JavaScript_L5_plus_L6.js
@@ -0,0 +1,19 @@
+// let num="4"
+
+// console.log(num+" "+typeof num);
+// n=Number(num)
+// console.log(n+" "+typeof n);
+
+let num2="66abxx"
+// coversion of type is allowed in javascript but the value if not valid then different
+// behaviour is observed like following
+
+// let num3=Number(num2)
+// console.log(typeof num3+" "+num3); //NaN  (not a number)
+
+// -- "" empty string is by default false and any non empty string is true
+console.log(+true);//1 is output plus converts true to number 1
+
+
+console.log();
+
