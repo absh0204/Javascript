a048e607151e2adbcfedcd7faead01c0102ffd0a
Atul Bharadwaj <tulbha2482@gmail.com>
2024-03-02 11:19:31 +0530

    functions completed

---
+++ 1.Variables/Functions.js
@@ -0,0 +1,45 @@
+function say(){
+    console.log("I");
+    console.log("a");
+    console.log("h");
+    console.log("d");
+    console.log("b");
+    console.log("A");
+}
+
+// say()//to execute
+ function add(number1,number2){
+    console.log(number1+number2);
+ }
+ function add(number1,number2){
+    console.log(number1+number2);
+    return number1+number2
+}
+//  console.log(3,4);//7
+//  console.log(3,"4");//34
+//  console.log(3,"a");//3a
+
+function login(username="sam"){//minimum sam is argument it is overwritten if argument is passed
+    return `${username} just logged in`
+}
+
+
+//  if no. of argument is not known
+// rest operator (...num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
